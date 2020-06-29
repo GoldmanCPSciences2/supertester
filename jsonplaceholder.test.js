4b47e00d66015b75c7fe641679fcce92b11a41dc
Andrew <andrew.goldman00@gmail.com>
2020-06-29 09:58:52 -0600

    first commit

---
+++ jsonplaceholder.test.js
@@ -0,0 +1,30 @@
+const request = require("supertest");
+const url = request("https://jsonplaceholder.typicode.com");
+
+describe("json placeholder api test", () => {
+  let data = {
+    stuff: 27,
+    otherstuff: "stupid stuff",
+    morestuff: 2.7,
+  };
+
+  it("can get a comment", (done) => {
+    url.get("/posts/1/comments").expect(200, done);
+  });
+
+  it("can get some photos", (done) => {
+    url.get("/albums/1/photos").expect(200, done);
+  });
+
+  it("can get a users albums", (done) => {
+    url.get("/users/1/albums").expect(200, done);
+  });
+
+  it("can get a to do list", (done) => {
+    url.get("/users/1/todos").expect(200, done);
+  });
+
+  it("can get a post", (done) => {
+    url.get("/users/1/posts").expect(200, done);
+  });
+});
